571a9a1582bc50428e478de5dbdb9ae9e4daf121
VIbhav Pol <vibhavpol1477@gmail.com>
2025-10-10 19:13:53 +0530

    init commit

--- link getter.py
+++ link getter.py
@@ -1,6 +1,40 @@
+import validators
+
 def get_link():
     url = input("Enter the URL: ")
     return url
 
+
+
+def is_valid_url(url):
+    if validators.url(url):
+        return True
+    else:
+        return False
+
+def check_suspicious_pattern(url):
+    suspicious_keywords = ['secure', 'login', 'update', 'verify', 'account', 'confirm', 'banking', 'password']
+    url_lower = url.lower()
+    found_keywords = []
+
+    for keywords in suspicious_keywords:
+        if keywords in url_lower:
+            found_keywords.append(keywords)
+
+    if found_keywords:
+        print(f"Warning: The URL contains suspicious keywords:")
+        return True
+    else:
+        print(f"Warning: The URL does not contain suspicious keywords:")
+        return False
+
+
+
+
+
 link = get_link()
-print(f"You entered: {link}")
+if is_valid_url(link):
+    print(f"The URL '{link}' is valid.")
+    check_suspicious_pattern(link)
+else:
+    print(f"The URL '{link}' is not valid.")
